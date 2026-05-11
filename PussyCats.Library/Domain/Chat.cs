e16bc5a483c78c21a72e123885429a2502c0d0cd
anaarebeca <rebeca.ana@stud.ubbcluj.ro>
2026-05-12 00:04:19 +0300

    chats no longer ignored

---
+++ PussyCats.Library/Domain/Chat.cs
@@ -0,0 +1,21 @@
+namespace PussyCats.Library.Domain;
+using System.Text.Json.Serialization;
+
+public class Chat
+{
+    public int ChatId { get; set; }
+    [JsonIgnore] public User User { get; set; } = null!;
+    public int? CompanyId { get; set; }
+    [JsonIgnore] public Company? Company { get; set; }
+    public int? SecondUserId { get; set; }
+    public int? JobId { get; set; }
+    public bool IsBlocked { get; set; }
+    public int? BlockedByUserId { get; set; }
+    public DateTime? DeletedAtByUser { get; set; }
+    public DateTime? DeletedAtBySecondParty { get; set; }
+    public string LastMessageSnippet { get; set; } = string.Empty;
+    public string LastMessageTime { get; set; } = string.Empty;
+    public string LastMessage { get; set; } = string.Empty;
+    public int UnreadCount { get; set; }
+    public string OtherPartyName { get; set; } = string.Empty;
+}
