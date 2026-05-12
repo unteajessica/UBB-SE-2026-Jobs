416da87918085d629df16338e4ac4a3712f66a5d
aldenisa <d3157190@gmail.com>
2026-05-12 12:15:04 +0300

    removed JobId, SkillId and my connection string

--- PussyCats.Library/Domain/JobSkill.cs
+++ PussyCats.Library/Domain/JobSkill.cs
@@ -4,10 +4,7 @@ namespace PussyCats.Library.Domain;
 
 public class JobSkill
 {
-    //public int JobId { get; set; }
     public Job Job { get; set; } = null!;
-
-    //public int SkillId { get; set; }
 
     public Skill Skill { get; set; } = null!;
 
