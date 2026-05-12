7c80ef53cccbf00a5130166fc5643f3a36132dbe
aldenisa <d3157190@gmail.com>
2026-05-12 20:11:29 +0300

    deleted test in SkillTestService

--- PussyCats.Tests/Services/SkillTestServiceTests.cs
+++ PussyCats.Tests/Services/SkillTestServiceTests.cs
@@ -84,25 +84,6 @@ public class SkillTestServiceTests
 
     }
 
-    [Fact]
-    public async Task SubmitRetakeAsync_EligibleTest_UpdatesScore()
-    {
-        int skillTestId = 1;
-        int initialScore = 40;
-        int newScore = 95;
-        DateOnly sixMonthsAgo = DateOnly.FromDateTime(DateTime.Now.AddMonths(-6));
-        skillTestRepository.Seed(new SkillTestBuilder()
-            .WithId(skillTestId)
-            .WithScore(initialScore)
-            .WithAchievedDate(sixMonthsAgo)
-            .Build());
-
-        var badgeResult = await skillTestService.SubmitRetakeAsync(skillTestId, newScore);
-
-        var updatedTest = await skillTestRepository.GetByIdAsync(skillTestId);
-        updatedTest!.Score.Should().Be(newScore);
-    }
-
 
     [Fact]
     public async Task SubmitRetakeAsync_NotYetEligible_ThrowsException()
