40358050b09d751633b51f57de5677ee80e85904
berintanemanuel <natiberintan3@gmail.com>
2026-05-13 11:12:22 +0300

    Add tests for AddPost method in DeveloperService

---
+++ PussyCats.Tests/Services/DeveloperServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using NSubstitute.ExceptionExtensions;
+using PussyCats.App.Services;
+using PussyCats.Library.Domain;
+using PussyCats.Library.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PussyCats.Tests.Services
+{
+    public class DeveloperServiceTests
+    {
+        private DeveloperService developerService = new DeveloperService();
+
+        [Fact]
+        public void AddPost_UnknownPostParameterType_ThrowsArgumentException()
+        {
+            Developer developer = new Developer
+            {
+                DeveloperId = 1,
+                Name = "John"
+            };
+            string postMessage = "Hello world!";
+            var exception = Assert.Throws<ArgumentException>(() => developerService.AddPost(developer.DeveloperId, DeveloperPostParameterType.Unknown, postMessage));
+            exception.Message.Should().Contain("Choose a valid parameter.");
+        }
+
+        [Fact]
+        public void AddPost_EmptyMessage_ThrowsArgumentException()
+        {
+            Developer developer = new Developer
+            {
+                DeveloperId = 1,
+                Name = "John"
+            };
+            string postMessage = string.Empty;
+            var exception = Assert.Throws<ArgumentException>(() => developerService.AddPost(developer.DeveloperId, DeveloperPostParameterType.WeightedDistanceScoreWeight, postMessage));
+            exception.Message.Should().Contain("Value cannot be empty.");
+        }
+
+        [Fact]
+        public void AddPost_ValidInput_AddsPostSuccessfully()
+        {
+            Developer developer = new Developer
+            {
+                DeveloperId = 1,
+                Name = "John"
+            };
+            string postMessage = "Hello world!";
+            var post = developerService.AddPost(developer.DeveloperId, DeveloperPostParameterType.WeightedDistanceScoreWeight, postMessage);
+
+            post.Developer.DeveloperId.Should().Be(developer.DeveloperId);
+            post.Value.Should().Be(postMessage);
+        }
+    }
+}
