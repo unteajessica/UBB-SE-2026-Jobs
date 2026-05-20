98621eb4ef50658cd523f1d5aa2b65c6604b12ed
xaleamo <xaleamo@gmail.com>
2026-05-21 01:34:26 +0300

    feat:  added debug helper - writes to file

---
+++ PussyCats.Library/Helpers/DebugToFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PussyCats.Library.Helpers
+{
+    public static class DebugToFile
+    {
+        public static void Write(string type, string message)
+        {
+            try
+            {
+                string path = "C:\\PERSONALE\\Desktop\\iss.log";
+
+                if (!System.IO.File.Exists(path))
+                {
+                    return;
+                }
+
+                System.IO.File.AppendAllText(path, $"{type}  -  {message}{Environment.NewLine}");
+            }
+            catch
+            {
+                // Ignore any exceptions that occur while writing to the file
+            }
+        }
+    }
+}
