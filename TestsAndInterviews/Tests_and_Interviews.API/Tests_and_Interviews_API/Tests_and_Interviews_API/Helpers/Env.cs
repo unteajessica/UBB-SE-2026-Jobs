8814c6728dbcb472010d0f698f5d782638f1407f
Andrei-Darius Bodrogean <bodrogean.andrei@gmail.com>
2026-06-03 22:50:05 +0300

    done merge conflicts

--- TestsAndInterviews/Tests_and_Interviews.API/Tests_and_Interviews_API/Tests_and_Interviews_API/Helpers/Env.cs
+++ TestsAndInterviews/Tests_and_Interviews.API/Tests_and_Interviews_API/Tests_and_Interviews_API/Helpers/Env.cs
@@ -2,7 +2,7 @@
 {
     public static class Env
     {
-        public const string CONNECTION_STRING = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ISS_Merged;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        public const string CONNECTION_STRING = "Server=DESKTOP-M6HSOV2;Database=ISS_Merged;Trusted_Connection=True;TrustServerCertificate=True;";
         public static int RECRUITER_ID = 1;
         public static int USER_ID = 1;
     }
