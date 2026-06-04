f22a48669e8f3234409b7c0893eea31662c4784d
aldenisa <d3157190@gmail.com>
2026-06-04 22:41:09 +0300

    removed fluent asserts

--- TestsAndInterviews/Tests_and_Interviews.API/Tests_and_Interviews_API/Tests_and_Interviews_API/Helpers/Env.cs
+++ TestsAndInterviews/Tests_and_Interviews.API/Tests_and_Interviews_API/Tests_and_Interviews_API/Helpers/Env.cs
@@ -2,7 +2,7 @@
 {
     public static class Env
     {
-        public const string CONNECTION_STRING = "Data Source=DESKTOP-M6HSOV2;Database=ISS_Merged;Trusted_Connection=True;TrustServerCertificate=True;";
+        public const string CONNECTION_STRING = "Data Source=DESKTOP-MDHK46B\\SQLEXPRESS;Database=ISS_Merged;Trusted_Connection=True;TrustServerCertificate=True;";
         public static int RECRUITER_ID = 1;
         public static int USER_ID = 1;
     }
